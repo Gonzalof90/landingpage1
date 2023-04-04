7cc51cec05eb01bafc99d2d61565591977085a4b
Michael Jurado <mikjurado1@gmail.com>
2023-04-04 21:37:47 +0200

    inicio de proyecto

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,36 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import logo from '../assets/images/logo.png'
+
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <Container>
+        <Row className='mt-3 text-center'>
+          <Col md-auto><img src={logo} alt='logo'/></Col>
+          <Col xs={0} md-auto className='square border-end'>Features</Col>
+          <Col xs={2} className='square border-end'>Plans and Pricing</Col>
+          <Col xs={0} className='square border-end'>Blog</Col>
+          <Col xs={3} className='text-start' >Resources</Col>
+          <Col>Sign up</Col>
+          <Col>
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                Login
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default Navbar
